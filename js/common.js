81b7b99fea2632784724dded7dd26ac55e96557d
Dmitry-CH <dmit.checkanov@yandex.ru>
2016-06-23 17:57:27 +0300

    Вставка адаптивной графики, добавлена вся графика

--- js/common.js
+++ js/common.js
@@ -1,14 +1,22 @@
+var html = document.querySelector('.no-js');
+
+html.classList.remove('no-js');
+
+
 var button = document.querySelector('.page-header__toggle'),
     menu   = document.querySelector('.main-nav');
 
 button.addEventListener('click', function(event) {
   event.preventDefault();
+
   var check = menu.classList.contains('main-nav--closed');
 
   if(check) {
+    button.classList.add('page-header__toggle--open');
     menu.classList.remove('main-nav--closed');
   }
   else {
+    button.classList.remove('page-header__toggle--open');
     menu.classList.add('main-nav--closed');
-  }
+  };
 });
